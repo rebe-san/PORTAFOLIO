5abab63ef26d46f906b04ce5067c61a30bedf65e
Rebeca Chiñas <chi.san.rebeca@gmail.com>
2019-11-23 14:29:58 -0600

    Updating styles

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 // script efecto scroll smooth
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
     });
   });
-  
+});
